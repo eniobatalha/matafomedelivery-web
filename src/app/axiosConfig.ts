ad7989a37b384aab4b071c89ae0fe4490664bf0b
eniobatalha <eniobatalha@gmail.com>
2024-08-25 13:56:11 -0300

    axiosConfig atualizado para https no lugar de http

--- src/app/axiosConfig.ts
+++ src/app/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://matafome-api.ashyfield-34914be1.brazilsouth.azurecontainerapps.io/api',
+    baseURL: 'https://matafome-api.ashyfield-34914be1.brazilsouth.azurecontainerapps.io/api',
     headers: {
         'Content-Type': 'application/json',
     },
