8c9f66cf1d47b6088c458c9cc4caae0af14bea81
eniobatalha <eniobatalha@gmail.com>
2024-09-01 15:15:47 -0300

    Versão DEV apontando para server pocket

--- src/app/axiosConfig.ts
+++ src/app/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://matafome-api.whiteglacier-7456d729.brazilsouth.azurecontainerapps.io/api',
+    baseURL: 'http://168.196.221.89:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
